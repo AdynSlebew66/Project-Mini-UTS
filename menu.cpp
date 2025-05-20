73f69ee58b9853402b260cefbfbe1c3d31db3785
Zidan <mr.zidan41@gmail.com>
2025-05-20 14:05:29 +0800

    InsertSortDescending

--- menu.cpp
+++ menu.cpp
@@ -65,3 +65,17 @@ void insertionSortAscending(vector<int>& arr){
       arr[j+1] = key;
   }
 }
+
+void insertionSortDescending(vector<int>& arr){
+  int n = arr.size();
+  for(int i = 1; i < n; i++){
+      int key = arr[i];
+      int j = i - 1;
+      
+      while(j >= 0 && arr[j] < key){
+          arr[j+1] = arr[j];
+          j--;
+      }
+      arr[j+1] = key;
+  }
+}
